de556511554d374e1255b8c04071cd3bf49f502b
lisuheng <lisuheng2022@163.com>
2024-08-02 18:58:49 +0800

    feat: 启动MySQL的时候，执行指定的SQL

--- mysql/mysql-schema.sql
+++ mysql/mysql-schema.sql
@@ -208,6 +208,7 @@ CREATE TABLE `permissions` (
     UNIQUE INDEX `uk_role_permission` (`role`,`resource`,`action`) USING BTREE
 );
 
+-- nacos/nacos
 INSERT INTO users (username, password, enabled) VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
 
 INSERT INTO roles (username, role) VALUES ('nacos', 'ROLE_ADMIN');
