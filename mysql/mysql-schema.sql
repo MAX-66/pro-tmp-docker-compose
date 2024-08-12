9cb33f46c9b0c66a62eb176331bca07aba95ecb6
lxq <luoxinqiong@pinlor.com>
2024-08-12 22:21:09 +0800

    seata

--- mysql/mysql-schema.sql
+++ mysql/mysql-schema.sql
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+CREATE DATABASE nacos;
 
+use nacos;
 /******************************************/
 /*   表名称 = config_info                  */
 /******************************************/
